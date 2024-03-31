dd070055284b836371c001eb1fa3bcad667c9958
Hridoy Islam <hridoy4t@gmail.com>
2024-03-31 10:42:07 +0600

    triyaksh

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,29 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
-
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
 
 function Home({ posts }) {
   return (
     <Layout home>
-        <Head>
-        <title>{siteTitle}</title>
+      <Head>
+        <title>Mother Fucker</title>
       </Head>
-      <section >
-        <h1>Welcome to Next.js!</h1>
-        <ul>
-        {posts.map((post) => (
-          <li key={post.id}> 
-            <Link href={'/blog/posts/'+post.id} >{post.title}
-            </Link>
-          </li>
-        
-      ))}
-          
-        </ul>
+      <section>
+        <h1>This Website Owner is a theif. please don't work for him</h1>
       </section>
     </Layout>
-    
-  )
+  );
 }
 
-export async function getStaticProps(){
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-  const posts = await res.json()
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
 
   return {
-    props : {
+    props: {
       posts,
     },
-  }
+  };
 }
 
-export default Home
+export default Home;
